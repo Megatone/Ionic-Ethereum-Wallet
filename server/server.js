79625aa3ed2de11be95d7e73b54757b0bb406801
Megatone <inacio_pk2@hotmail.com>
2017-08-26 19:24:57 +0200

    Comment

--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var port = 8000;
-app.use(express.static('www'));
-server.listen(port, function () {
-    console.log("Iniciando Servidor Puerto : " + port);
-});
